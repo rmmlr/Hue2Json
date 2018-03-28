23ecd035ada91bbdf9911848e2da901c43140a92
Elias Ruemmler <git@rc-art.de>
2018-03-28 11:50:32 +0200

    Xml Beschreibung ergänzt

--- HueParameterEnum.cs
+++ HueParameterEnum.cs
@@ -13,24 +13,54 @@ namespace Rca.Hue2Json
     [Flags]
     public enum HueParameterGroupEnum
     {
+        /// <summary>
+        /// Leuchtmittel
+        /// </summary>
         [Display(Name = "Leuchtmittel")]
         Lights = 2,
+        /// <summary>
+        /// Gruppen
+        /// </summary>
         [Display(Name = "Gruppen")]
         Groups = 4,
+        /// <summary>
+        /// Timer
+        /// </summary>
         [Display(Name = "Timer")]
         Schedules = 8,
+        /// <summary>
+        /// Szenen
+        /// </summary>
         [Display(Name = "Szenen")]
         Scenes = 16,
+        /// <summary>
+        /// Sensoren
+        /// </summary>
         [Display(Name = "Sensoren")]
         Sensors = 32,
+        /// <summary>
+        /// Regeln
+        /// </summary>
         [Display(Name = "Regeln")]
         Rules = 64,
+        /// <summary>
+        /// Konfiguration
+        /// </summary>
         [Display(Name = "Konfiguration")]
         Configuration = 128,
+        /// <summary>
+        /// Links
+        /// </summary>
         [Display(Name = "Links")]
         ResourceLinks = 256,
+        /// <summary>
+        /// Speicher-Belegung
+        /// </summary>
         [Display(Name = "Speicher-Belegung")]
         Capability = 512,
+        /// <summary>
+        /// Benutzerliste
+        /// </summary>
         [Display(Name = "Benutzerliste")]
         WhiteList = 1024
     }
@@ -71,6 +101,11 @@ namespace Rca.Hue2Json
 
     public static class DisplayExtensions
     {
+        /// <summary>
+        /// Ruft den menschenlesbaren Name ab
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static string DisplayName(this Enum value)
         {
             var field = value.GetType().GetField(value.ToString());
