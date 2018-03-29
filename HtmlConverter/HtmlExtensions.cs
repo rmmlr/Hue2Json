78b3a93a937cb900d57264eeb94d76c7799acfa6
Elias Ruemmler <git@rc-art.de>
2018-03-29 18:27:18 +0200

    Add HtmlExtensions.cs

---
+++ HtmlConverter/HtmlExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.Hue2Json.HtmlConverter
+{
+    public static class HtmlExtensions
+    {
+        #region Member
+
+
+        #endregion Member
+
+        #region Properties
+
+
+        #endregion Properties
+
+        #region Services
+
+        /// <summary>
+        /// Serialisierung aller Properties ins HTML Format
+        /// </summary>
+        public static string ToHtml(this object obj, HtmlOutputFormat format)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Services
+
+        #region Internal services
+
+
+        #endregion Internal services
+
+        #region Events
+
+
+        #endregion Events
+    }
+
+    public enum HtmlOutputFormat
+    {
+        SortedList,
+        UnsortetList,
+    }
+}
