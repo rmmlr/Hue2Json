6791f22f829f4f2123fd0e2a4196c55f65b1a817
Elias Ruemmler <git@rc-art.de>
2018-04-03 15:32:38 +0200

    Add IdPair.cs

---
+++ Remapping/DeviceCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.Hue2Json.Remapping
+{
+    public enum DeviceCategory
+    {
+        Bridge = 1,
+        Light = 2,
+        Sensor = 3,
+    }
+}
---
+++ Remapping/IdPair.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.Hue2Json.Remapping
+{
+    public class IdPair
+    {
+        #region Member
+
+
+        #endregion Member
+
+        #region Properties
+        /// <summary>
+        /// Gerätetyp
+        /// </summary>
+        public DeviceCategory Category { get; set; }
+
+        /// <summary>
+        /// Von der Bridge vergebene ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Hardware-ID
+        /// </summary>
+        public string UniqueId { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+        /// <summary>
+        /// Empty constructor for IdPair
+        /// </summary>
+        public IdPair()
+        {
+
+        }
+
+        #endregion Constructor
+
+        #region Services
+
+
+        #endregion Services
+
+        #region Internal services
+
+
+        #endregion Internal services
+
+        #region Events
+
+
+        #endregion Events
+    }
+}
