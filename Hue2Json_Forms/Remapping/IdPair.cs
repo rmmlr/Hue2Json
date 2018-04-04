0f2bf14064e12fbb117ed0a89329b9407ac6965e
Elias Ruemmler <git@rc-art.de>
2018-04-04 15:55:27 +0200

    DebuggerDisplay

--- Hue2Json_Forms/Remapping/IdPair.cs
+++ Hue2Json_Forms/Remapping/IdPair.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rca.Hue2Json.Remapping
 {
+    [DebuggerDisplay("UID={UniqueId}, CurID={CurrentId}, BakID={BackupId}")]
     public class IdPair
     {
         #region Member
