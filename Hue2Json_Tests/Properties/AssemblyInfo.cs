f1a78dd262a639b1ea7a55b7fc75242dd3369bb5
Elias Ruemmler <git@rc-art.de>
2018-04-04 15:55:43 +0200

    Add Hue2Json_Tests

---
+++ Hue2Json_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hue2Json_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Hue2Json_Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3825575-352a-4621-b698-439369fac3f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Hue2Json_Tests/Remapping/RemappingInfo_Tests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Q42.HueApi;
+using Q42.HueApi.Models;
+using Rca.Hue2Json;
+using Rca.Hue2Json.Remapping;
+
+namespace Hue2Json_Tests
+{
+	[TestClass]
+	public class RemappingInfo_Tests
+	{
+		bool idPairComparer(IdPair a, IdPair b)
+		{
+			return String.Equals(a.UniqueId, b.UniqueId, StringComparison.InvariantCultureIgnoreCase)
+				&& String.Equals(a.CurrentId, b.CurrentId, StringComparison.InvariantCultureIgnoreCase)
+				&& String.Equals(a.BackupId, b.BackupId, StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		[TestMethod]
+		[TestCategory("RemappingInfo")]
+		public void Create()
+		{
+			var backup = new HueParameters()
+			{
+				Lights = new List<Light>()
+				{
+					new Light()
+					{
+						Id = "1",
+						UniqueId = "uid001"
+					},
+					new Light ()
+					{
+						Id = "2",
+						UniqueId = "uid002"
+					},
+					new Light ()
+					{
+						Id = "3",
+						UniqueId = "uid003"
+					}
+				},
+				Sensors = new List<Sensor>()
+				{
+					new Sensor()
+					{
+						Id = "1",
+						UniqueId = "uid001"
+					},
+					new Sensor()
+					{
+						Id = "2",
+						UniqueId = "uid002"
+					}
+				}
+			};
+
+			var current = new HueParameters()
+			{
+				Lights = new List<Light>()
+				{
+					new Light()
+					{
+						Id = "2",
+						UniqueId = "uid001"
+					},
+					new Light ()
+					{
+						Id = "3",
+						UniqueId = "uid002"
+					},
+					new Light ()
+					{
+						Id = "4",
+						UniqueId = "uid003"
+					}
+				},
+				Sensors = new List<Sensor>()
+				{
+					new Sensor()
+					{
+						Id = "2",
+						UniqueId = "uid001"
+					}
+				}
+			};
+
+			var expectedResult = new RemappingInfo
+			{
+				Lights = new List<IdPair>()
+				{
+					new IdPair()
+					{
+						BackupId = "1",
+						CurrentId = "2",
+						UniqueId = "uid001"
+					},
+					new IdPair
+					{
+						BackupId = "2",
+						CurrentId = "3",
+						UniqueId = "uid002"
+					},
+					new IdPair
+					{
+						BackupId = "3",
+						CurrentId = "4",
+						UniqueId = "uid003"
+					}
+				},
+				Sensors = new List<IdPair>
+				{
+					new IdPair
+					{
+						BackupId = "1",
+						CurrentId = "2",
+						UniqueId = "uid001"
+					},
+					new IdPair
+					{
+						BackupId = "2",
+						CurrentId = null,
+						UniqueId = "uid002"
+					}
+				}
+			};
+
+			var testObj = RemappingInfo.Create(backup, current);
+
+			Assert.AreEqual(expectedResult.Lights.Count, testObj.Lights.Count, "Anzahl der gemappten Light-IDs fehlerhaft");
+			Assert.AreEqual(expectedResult.Sensors.Count, testObj.Sensors.Count, "Anzahl der gemappten Sensor-IDs fehlerhaft");
+
+			for (int i = 0; i < expectedResult.Lights.Count; i++)
+				Assert.IsTrue(idPairComparer(expectedResult.Lights[i], testObj.Lights[i]), "Gemappte Light-IDs fehlerhaft");
+			for (int i = 0; i < expectedResult.Sensors.Count; i++)
+				Assert.IsTrue(idPairComparer(expectedResult.Sensors[i], testObj.Sensors[i]), "Gemappte Sensor-IDs fehlerhaft");
+
+		}
+	}
+}
