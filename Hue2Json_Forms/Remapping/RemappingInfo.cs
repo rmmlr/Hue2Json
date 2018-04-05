1b8af73a8a128314488387bbb11c1be6549d1421
Elias Ruemmler <git@rc-art.de>
2018-04-05 15:48:29 +0200

    Methoden zur ID Abfrage

--- Hue2Json_Forms/Remapping/RemappingInfo.cs
+++ Hue2Json_Forms/Remapping/RemappingInfo.cs
@@ -77,10 +77,54 @@ namespace Rca.Hue2Json.Remapping
             return result;
         }
 
+        /// <summary>
+        /// Gibt die neue ID zur übergebenen Backup-ID eines Leuchtmittels zurück
+        /// </summary>
+        /// <param name="backupId">Backup-ID eines Leuchtmittels</param>
+        /// <returns>Neue ID des Leuchtmittels</returns>
+        /// <exception cref="RemappingException">Mehrere Einträge zur übergebenen Backup-ID gefunden.</exception>
+        /// <exception cref="CurrentIdNotFoundException">Keinen Eintrag zur übergebenen Backup-ID gefunden.</exception>
+        /// <exception cref="EmptyIdMapException">Keine Mapping-Information verfügbar. ID-Map ist leer.</exception>
+        public string GetNewLightId(string backupId)
+        {
+            return getNewId(backupId, Lights);
+        }
+
+        /// <summary>
+        /// Gibt die neue ID zur übergebenen Backup-ID eines Sensors oder Schalters zurück
+        /// </summary>
+        /// <param name="backupId">Backup-ID eines Sensors oder Schalters</param>
+        /// <returns>Neue ID des Sensors oder Schalters</returns>
+        /// <exception cref="RemappingException">Mehrere Einträge zur übergebenen Backup-ID gefunden.</exception>
+        /// <exception cref="CurrentIdNotFoundException">Keinen Eintrag zur übergebenen Backup-ID gefunden.</exception>
+        /// <exception cref="EmptyIdMapException">Keine Mapping-Information verfügbar. ID-Map ist leer.</exception>
+        public string GetNewSensorId(string backupId)
+        {
+            return getNewId(backupId, Sensors);
+        }
+
         #endregion Services
 
         #region Internal services
+        string getNewId(string backupId, List<IdPair> idMap)
+        {
+            if (idMap?.Count > 0)
+            {
+                var idCount = idMap.Count(x => x.BackupId == backupId);
 
+                switch (idCount)
+                {
+                    case 0:
+                        throw new CurrentIdNotFoundException("Keinen Eintrag zur übergebenen Backup-ID (" + backupId + ") gefunden.");
+                    case 1:
+                        return idMap.First(x => x.BackupId == backupId).CurrentId;
+                    default:
+                        throw new RemappingException("Meherere Einträge zur übergebenen Backup-ID (" + backupId + ") gefunden.");
+                }
+            }
+            else
+                throw new EmptyIdMapException("Keine Mapping-Information verfügbar. ID-Map ist leer.");
+        }
 
         #endregion Internal services
 
