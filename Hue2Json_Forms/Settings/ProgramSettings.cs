734fe893fefa2612c09b6702e6023f6ed85707c1
Elias Ruemmler <git@rc-art.de>
2018-04-06 11:21:03 +0200

    Add ProgramSettings.cs

---
+++ Hue2Json_Forms/Settings/ProgramSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.Hue2Json.Settings
+{
+    public class ProgramSettings
+    {
+        #region Member
+
+
+        #endregion Member
+
+        #region Properties
+        public BridgeNameDisplayEnum BridgeNameDisplay { get; set; }
+
+        #endregion Properties
+
+        #region Constructor
+        /// <summary>
+        /// Empty constructor for Settings
+        /// </summary>
+        public ProgramSettings()
+        {
+
+        }
+
+        #endregion Constructor
+
+        #region Services
+
+
+        #endregion Services
+
+        #region Internal services
+
+
+        #endregion Internal services
+
+        #region Events
+
+
+        #endregion Events
+    }
+}
