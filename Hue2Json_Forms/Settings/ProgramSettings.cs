af757eedbb991b4f09b12268213ec3530881e5b1
Elias Ruemmler <git@rc-art.de>
2018-04-12 11:09:42 +0200

    Add Serializer Methods

--- Hue2Json_Forms/Settings/ProgramSettings.cs
+++ Hue2Json_Forms/Settings/ProgramSettings.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Reflection;
 
 namespace Rca.Hue2Json.Settings
 {
+    [Serializable]
     public class ProgramSettings
     {
         #region Member
@@ -14,6 +18,9 @@ namespace Rca.Hue2Json.Settings
         #endregion Member
 
         #region Properties
+
+        public Version Version { get; set; }
+
         public BridgeNameDisplayEnum BridgeNameDisplay { get; set; }
 
         #endregion Properties
@@ -30,7 +37,64 @@ namespace Rca.Hue2Json.Settings
         #endregion Constructor
 
         #region Services
+        /// <summary>
+        /// Serialisieren der Objektinstanz, als XML-Datei
+        /// </summary>
+        /// <param name="path">Dateipfad</param>
+        public void ToFile(string path)
+        {
+            if (!path.EndsWith(".config"))
+                throw new ArgumentException("Falsche Dateierweiterung der Einstellungsdatei, die Erweiterung muss *.config sein.");
 
+            try
+            {
+                if (File.Exists(path))
+                    File.Move(path, path.Substring(0, path.Length - 7) + "bak");
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Vorhandene Einstellungsdatei kann nicht überschrieben werden.", ex);
+            }
+            //Neue Einstellungsdatei erzeugen
+            using (FileStream fs = File.Create(path))
+            {
+                //Aktuelle Version eintragen
+                Version = typeof(ProgramSettings).Assembly.GetName().Version;
+
+                var xs = new XmlSerializer(typeof(ProgramSettings));
+
+                xs.Serialize(fs, this);
+                fs.Close();
+            }
+        }
+
+        /// <summary>
+        /// Deserialisieren einer Einstellungsdatei
+        /// </summary>
+        /// <param name="path">Dateipfad</param>
+        /// <returns>ProgramSettings Objekt</returns>
+        public static ProgramSettings FromFile(string path)
+        {
+            if (File.Exists(path))
+                throw new FileNotFoundException("Die angegebene Einstellungsdatei (" + path + ") konnte nicht gefunden werden.");
+
+            
+            var fs = new FileStream(path, FileMode.Open);
+            var xs = new XmlSerializer(typeof(ProgramSettings));
+
+            var settings = (ProgramSettings)xs.Deserialize(fs);
+
+            return settings;
+        }
+
+        public static ProgramSettings CreateDefault()
+        {
+            return new ProgramSettings
+            {
+                Version = typeof(ProgramSettings).Assembly.GetName().Version,
+                BridgeNameDisplay = BridgeNameDisplayEnum.NameAndIp
+            };
+        }
 
         #endregion Services
 
