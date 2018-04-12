685c3520a0963803c30f0ca4a7974cf8ca05701f
Elias Ruemmler <git@rc-art.de>
2018-04-12 14:16:08 +0200

    Add BridgeButtonView

---
+++ Hue2Json_Forms/View/BridgeButtonView.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Rca.Hue2Json.View
+{
+    partial class BridgeButtonView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.txt_Message = new System.Windows.Forms.TextBox();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 161);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(322, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 125);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_Message
+            // 
+            this.txt_Message.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_Message.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Message.Location = new System.Drawing.Point(165, 16);
+            this.txt_Message.Multiline = true;
+            this.txt_Message.Name = "txt_Message";
+            this.txt_Message.ReadOnly = true;
+            this.txt_Message.Size = new System.Drawing.Size(169, 125);
+            this.txt_Message.TabIndex = 3;
+            this.txt_Message.Text = "[Multiline text]";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(259, 203);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 4;
+            this.btn_Cancel.Text = "Abbrechen";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // BridgeButtonView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 243);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.txt_Message);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "BridgeButtonView";
+            this.Text = "BridgeButtonView";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox txt_Message;
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
---
+++ Hue2Json_Forms/View/BridgeButtonView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rca.Hue2Json.View
+{
+    public partial class BridgeButtonView : Form
+    {
+        public BridgeButtonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
