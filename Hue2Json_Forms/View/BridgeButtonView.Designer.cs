045d47b8afb9a0655d360db1ec5944623b1c8aee
Elias Ruemmler <git@rc-art.de>
2018-04-13 11:21:19 +0200

    Update

--- Hue2Json_Forms/View/BridgeButtonView.Designer.cs
+++ Hue2Json_Forms/View/BridgeButtonView.Designer.cs
@@ -28,40 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.fontDialog1 = new System.Windows.Forms.FontDialog();
-            this.txt_Message = new System.Windows.Forms.TextBox();
             this.btn_Cancel = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // progressBar
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 161);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(322, 23);
-            this.progressBar1.TabIndex = 0;
+            this.progressBar.Location = new System.Drawing.Point(12, 161);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(322, 23);
+            this.progressBar.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 16);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(129, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            // 
-            // txt_Message
-            // 
-            this.txt_Message.BackColor = System.Drawing.SystemColors.Control;
-            this.txt_Message.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_Message.Location = new System.Drawing.Point(165, 16);
-            this.txt_Message.Multiline = true;
-            this.txt_Message.Name = "txt_Message";
-            this.txt_Message.ReadOnly = true;
-            this.txt_Message.Size = new System.Drawing.Size(169, 125);
-            this.txt_Message.TabIndex = 3;
-            this.txt_Message.Text = "[Multiline text]";
             // 
             // btn_Cancel
             // 
@@ -71,30 +61,44 @@
             this.btn_Cancel.TabIndex = 4;
             this.btn_Cancel.Text = "Abbrechen";
             this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(147, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 125);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod t" +
+    "empor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At ve" +
+    "ro eos et la via. ";
             // 
             // BridgeButtonView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(348, 243);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Cancel);
-            this.Controls.Add(this.txt_Message);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.progressBar);
             this.Name = "BridgeButtonView";
             this.Text = "BridgeButtonView";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.FontDialog fontDialog1;
-        private System.Windows.Forms.TextBox txt_Message;
         private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Hue2Json_Forms/View/BridgeButtonView.cs
+++ Hue2Json_Forms/View/BridgeButtonView.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,59 @@ namespace Rca.Hue2Json.View
 {
     public partial class BridgeButtonView : Form
     {
-        public BridgeButtonView()
+        #region Constants
+        const int TIMER_INTERVALL = 100;
+        #endregion
+
+        #region Member
+        CancellationTokenSource m_CancellationSource;
+        int m_TimeoutCounter;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Wartezeit für Link-Button Betätigung in Sekunden
+        /// </summary>
+        public int Timeout { get; set; }
+        #endregion
+
+        #region Constructor
+        public BridgeButtonView(CancellationTokenSource source)
         {
+            m_CancellationSource = source;
             InitializeComponent();
+
+            //label1.Text = "1st line" + Environment.NewLine + "another line...";
+
+            Timeout = 60;
+            timer.Interval = TIMER_INTERVALL;
+            timer.Start();
         }
+        #endregion
+
+        #region Internal services
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            m_CancellationSource.Cancel();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (Timeout <= 0)
+                throw new ArgumentException("Ungültiger Wert für Timeout.");
+
+            m_TimeoutCounter++;
+
+            int progressValue = (int)(m_TimeoutCounter / (double)(Timeout * 10 / TIMER_INTERVALL) );
+
+            if (progressValue > 100)
+            {
+                timer.Stop();
+                m_CancellationSource.Cancel();
+            }
+            progressBar.Value = progressValue;
+        }
+        #endregion
     }
 }
