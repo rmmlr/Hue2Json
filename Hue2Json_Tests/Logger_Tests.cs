a5e0a914005fa52b8711b73a0f8435218a3f0a0a
Elias Ruemmler <elias.ruemmler@icloud.com>
2018-04-20 19:20:26 +0200

    Test für Logger

---
+++ Hue2Json_Tests/Logger_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rca.Hue2Json;
+
+namespace Hue2Json_Tests
+{
+    [TestClass]
+    public class Logger_Tests
+    {
+        [TestMethod, TestCategory("Logger")]
+        public void WriteNewLog()
+        {
+            Logger.WriteToLog("test");
+        }
+    }
+}
